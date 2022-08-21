80c46a2253b0b29bb00f25af808d9fa07d05ec6a
Iskander <zdrogow@gmail.com>
2022-08-21 15:40:40 +0400

    3. Массивы и модели. [#174892]

---
+++ src/main/java/ru/job4j/pojo/Library.java
@@ -0,0 +1,33 @@
+package ru.job4j.pojo;
+
+public class Library {
+
+    public static void main(String[] args) {
+        Book horstmann = new Book("Core Java", 928);
+        Book martin = new Book("Clean Code", 464);
+        Book sierra = new Book("Head first Java", 720);
+        Book eckel = new Book("Thinking in Java", 1150);
+        Book[] books = new Book[4];
+        books[0] = horstmann;
+        books[1] = martin;
+        books[2] = sierra;
+        books[3] = eckel;
+        for (int i = 0; i < books.length; i++) {
+            Book pr = books[i];
+            System.out.println(pr.getName() + " " + pr.getNumbers());
+        }
+        Book temp = books[0];
+        books[0] = books[3];
+        books[3] = temp;
+        for (int i = 0; i < books.length; i++) {
+            Book pr = books[i];
+            System.out.println(pr.getName() + " " + pr.getNumbers());
+        }
+        for (int i = 0; i < books.length; i++) {
+            Book pr = books[i];
+            if (pr.getName().equals("Clean Code")) {
+                System.out.println(pr.getName());
+            }
+        }
+    }
+}
